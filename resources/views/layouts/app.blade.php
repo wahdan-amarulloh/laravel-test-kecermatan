52610b3235b898b0c0c7c3e5124f11c9e805a9fd
wahdam <wahdam.amarulloh@gmail.com>
2023-03-03 10:20:14 +0700

    add livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <livewire:styles />
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -42,6 +43,7 @@
         </div>
     </div>
 
+    <livewire:scripts />
     @stack('scripts')
 </body>
 
