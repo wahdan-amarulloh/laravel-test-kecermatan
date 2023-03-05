996d924ee94c016d47f13c5995366115e9ea8d70
wahdam <wahdam.amarulloh@gmail.com>
2023-03-05 10:20:29 +0700

    remove test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,12 +9,6 @@ class DashboardController extends Controller
         $users = \App\Models\User::query()->count();
         $histories = \App\Models\User::with('questions')->where('id', auth()->id())->get();
 
-        $question = \App\Models\Question::find(1);
-        $user = \App\Models\User::find(1);
-        $answer = 'B';
-
-        $user->questions()->attach($question->id, ['answer' => 'A','detail_id' => '1']);
-
         return view('dashboard')
         ->with('users', $users)
         ->with('histories', $histories);
