eb0a625f7e40877ca9bf4ae347f85bd5b75ec3a9
wahdam <wahdam.amarulloh@gmail.com>
2023-03-07 22:19:24 +0700

    add timer

--- resources/views/test/member.blade.php
+++ resources/views/test/member.blade.php
@@ -150,7 +150,7 @@
             document.addEventListener('alpine:init', () => {
                 Alpine.data('local', () => ({
                     storeAnswer: {},
-                    timeTest: 5,
+                    timeTest: 120,
                     running: false,
                     errorMessage: null,
                     questions_detail: [],
