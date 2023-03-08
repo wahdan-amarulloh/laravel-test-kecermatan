c7822282c5813fab66fa99e679146aa95034aa48
wahdam <wahdam.amarulloh@gmail.com>
2023-03-08 19:23:41 +0700

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -196,34 +196,6 @@
                                 </div>
                             </div>
                         @empty
-                            <p>kosong</p>
-                        @endforelse
-
-                        {{-- @forelse ($histories as $history)
-                            <div
-                                class="group flex cursor-pointer items-center gap-x-5 rounded-md px-2.5 py-2 transition-all duration-75 hover:bg-green-100">
-                                <div
-                                    class="flex h-12 w-12 items-center rounded-lg bg-gray-200 text-black group-hover:bg-green-200">
-                                    <span
-                                        class="tag w-full text-center text-2xl font-medium text-gray-700 group-hover:text-green-900">
-                                        <svg class="mx-auto h-6 w-6" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                            stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                                        </svg>
-
-                                    </span>
-                                </div>
-                                <div class="flex flex-col items-start justify-between font-light text-gray-600">
-                                    <p class="text-[15px]">{{ $history->test_at }}</p>
-                                    <span
-                                        class="flex items-center rounded border border-red-400 bg-white px-2 py-1 text-xs font-semibold text-red-400">
-                                        {{ $history->points }} : Points
-                                    </span>
-                                </div>
-                            </div>
-                        @empty
                             <div
                                 class="group flex cursor-pointer items-center gap-x-5 rounded-md px-2.5 py-2 transition-all duration-75 hover:bg-green-100">
                                 <div
@@ -244,7 +216,7 @@
                                     <span class="text-xs font-light text-gray-400">{{ now()->diffForHumans() }}</span>
                                 </div>
                             </div>
-                        @endforelse --}}
+                        @endforelse
                     </div>
                 </div>
             </x-card>
