0b83ba1806e6668d7432f1920c127df2450ea372
wahdam <wahdam.amarulloh@gmail.com>
2023-04-09 01:43:50 +0700

    try logout other device

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -53,6 +53,8 @@ class LoginRequest extends FormRequest
             ]);
         }
 
+        Auth::logoutOtherDevices($this->get('password'));
+
         RateLimiter::clear($this->throttleKey());
     }
 
